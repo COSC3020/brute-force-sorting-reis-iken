e215df0e900b18bd0fb8d19d19ac61459dd07b9a
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-23 23:17:59 -0600

    code.js

--- code.js
+++ code.js
@@ -1,14 +1,11 @@
 function permutationSort(a) {
-    let temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
     let length = a.length;
     let permutations = 0;
-    doPermutations(a, 0, a.length);
+    doPermutations(a, 0, a.length, permutations);
     return permutations;
 }
 
-function doPermutations(a, start, length) {
+function doPermutations(a, start, length, permutations) {
     if (start === length - 1) {
         permutations++;
         if (JSON.stringify(a) === JSON.stringify(a.slice().sort((x, y) => x - y))) {
@@ -18,10 +15,16 @@ function doPermutations(a, start, length) {
     }
     for (let i = start; i < length; i++) {
         swap(a, start, i);
-        if (doPermutations(a, start + 1, length)) {
+        if (doPermutations(a, start + 1, length, permutations)) {
             return true;
         }
         swap(a, start, i);
     }
     return false;
 }
+
+function swap(a, i, j) {
+        let temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
